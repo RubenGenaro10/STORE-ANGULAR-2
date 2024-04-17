8b749cfd2449a1eb090fac05a34f3d2b2cca1240
RubenGenaro10 <rubengenaro10@gmail.com>
2024-04-17 00:54:24 -0500

    sad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { FooterContentComponent } from './public/components/footer-content/footer-content.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ProductListComponent } from './store/components/product-list/product-list.component';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardModule
+} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterContentComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    MatCardImage,
+    MatCardModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
